a01e5a3970f1882b2739696da26ab1e894ff8497
吉会田 <jihuitian@souyidai.com>
2017-03-22 20:31:56 +0800

    资讯

---
+++ exchange/js/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ikuaiyue on 17-1-11.
+ */
+
+var getImgServer = "https://sjs.miaoa.com:1010/";                                   // 获取图片地址（正式）
+var uploadImgServerInterface = "https://sjs.miaoa.com/uusjs/fileupload.do"           // 上传图片接口（正式）
+//var getImgServer = "http://192.168.1.123/";                                         // 获取图片地址（测试）
+//var uploadImgServerInterface = "http://192.168.1.123:8090/uusjs/fileupload.do";     // 上传图片接口（测试）
